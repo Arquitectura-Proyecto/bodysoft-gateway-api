bc9af2e3d232957bc18a30f38cf2e386fa65fe8a
sv1024 <vejars1024@gmail.com>
2020-04-18 12:55:08 -0500

    data, resolvers and typedefs implemented

--- src/data/chat/chat.js
+++ src/data/chat/chat.js
@@ -3,19 +3,80 @@ import axios from "axios";
 
 export const getChatsTrainer = async (idTrainer) => {
     try {
-        const response = await axios.get(uriChatTrainer + '/' + idTrainer);
+        const response = await axios.get(uriChatTrainer + idTrainer);
         return response.data;
     } catch (error) {
         console.error(error);
     }
 }
 
-export const getChatsUser = async (idUser) => {
+export const getTrainerUserChat = async (trainerId, userId ) => {
     try {
-        const response = await axios.get(uriChatUser + '/' + idUser);
+        const response = await axios.get(uriChatTrainer + trainerId + '/' + userId);
         return response.data;
     }catch(error){
         console.log(error);
     }
-    
+}
+
+export const startTrainerUserChat = async (trainerId, userId) => {
+    try {
+        const response = await axios.post(uriChatTrainer + trainerId, {
+            id_user : userId
+        });
+        return response.data;
+    }catch(error){
+        console.log(error);
+    }
+}
+
+export const sendMessageUser = async (trainerId, chatId, messageContent) => {
+    try {
+        const response = await axios.post(uriChatTrainer  + trainerId + '/' + chatId, {
+            message : messageContent
+        });
+        return response.data;
+    }catch(error){
+        console.log(error);
+    }
+}
+
+
+export const getChatsUser = async (idUser) => {
+    try {
+        const response = await axios.get(uriChatUser  + idUser);
+        return response.data;
+    }catch(error){
+        console.log(error);
+    }
+}
+
+export const getUserTrainerChat = async (userId, trainerId) => {
+    try {
+        const response = await axios.get(uriChatUser  + userId + '/' + trainerId);
+        return response.data;
+    }catch(error){
+        console.log(error);
+    }
+}
+
+export const startUserTrainerChat = async (userId, trainerId) => {
+    try {
+        const response = await axios.post(uriChatUser + '/' + userId, {
+            id_trainer : trainerId
+        });
+        return response.data;
+    }catch(error){
+        console.log(error);
+    }
+}
+export const sendMessageTrainer = async (userId, chatId, messageContent) => {
+    try {
+        const response = await axios.post(uriChatUser + userId + '/' + chatId, {
+            message : messageContent
+        });
+        return response.data;
+    }catch(error){
+        console.log(error);
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import express, { response } from "express";
 import graphqlHTTP from "express-graphql";
 import schema from "./schema";
+import {startTrainerUserChat,startUserTrainerChat, sendMessageTrainer, sendMessageUser} from "./data/index"
 
 const app = express();
 
-app.set('port',process.env.PORT || 3000)
+app.set('port',process.env.PORT || 4000)
 
 app.use('/graphql',graphqlHTTP({
     graphiql:true,
     schema:schema
 }));
-
+//
+// startUserTrainerChat(1,3).then(e => {
+//     console.log(e)
+// })
+//
+// startTrainerUserChat(1,1).then(e => {
+//     console.log(e)
+// })
+//
+// sendMessageTrainer(1, "5e9b25dd6f269427b6075c3c", {content : "Helllo123!!!"} )
+//     .then(e =>{
+//     console.log(e)
+// })
+//
+// sendMessageUser(1, "5e9b27c86f269427b6075c3d", {content : "Test123!!!"} )
+//     .then(e =>{
+//         console.log(e)
+//     })
 app.listen(app.get('port'), ()=>console.log(`Server on port ${app.get('port')}`))

7f97822308e897226f1855b1d741752872de97f0
DanielaDuque <danduquegar@unal.edu.co>
2020-04-17 19:49:32 -0500

    :sparkles: Data

---
+++ src/data/session/session.js
@@ -0,0 +1,98 @@
+import { uriSession } from "./server";
+import axios from "axios";
+
+
+/*
+CREATE
+*/
+
+export const registerSchedule = async (schedule) => {
+    try {
+        const response = await axios.post(uriSession + '/schedule/create',schedule);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+/*
+DELETE
+*/
+
+export const deleteSchedule = async (ChageState) => {
+    try {
+        const response = await axios.delete(uriSession + '/schedule/delete',{ data: ChageState});
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+/*
+PUT
+*/
+export const setAdate = async (ChageState) => {
+    try {
+        const response = await axios.put(uriSession + '/set-a-date',ChageState);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const calcelUser = async (ChageState) => {
+    try {
+        const response = await axios.put(uriSession + '/cancel/user',ChageState);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const calcelCoach = async (ChageState) => {
+    try {
+        const response = await axios.put(uriSession + '/cancel/coach',ChageState);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+/*
+GET
+*/
+export const getAllbyIdCoach = async (idCoach) => {
+    try {
+        const response = await axios.get(uriSession + '/get-by-idCoach/' + idCoach);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+export const getAllbyCoachCurrent = async (idCoach) => {
+    try {
+        const response = await axios.get(uriSession + '/get-by-idCoach/Current/' + idCoach);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const getAllbyIdUser = async (idUser) => {
+    try {
+        const response = await axios.get(uriSession + '/get-by-idUser/' + idUser);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+export const getAllbyUserCurrent = async (idUser) => {
+    try {
+        const response = await axios.get(uriSession + '/get-by-idUser/Current/' + idUser);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}

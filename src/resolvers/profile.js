58227ba8781088aab116287b2e2061b5c6f6c942
Valentina <vbernalb@unal.edu.co>
2020-04-19 16:05:49 -0500

    con GETs

--- src/resolvers/profile.js
+++ src/resolvers/profile.js
@@ -1,6 +1,35 @@
+import * as Profile from "../data/profile/profile";
+
 const resolvers = {
     Query:{
-        
+        async profileUsers(_){
+            const response = await Profile.getProfileUsers();
+            return response;
+        },
+        async profileTrainers(_){
+            const response = await Profile.getProfileTrainers();
+            return response;
+        },
+        async profileDegreesByTrainers(_, {idTrainer}){
+            const response = await Profile.getDegreesByTrainers(idTrainer);
+            return response;
+        },
+        async profileSpecialities(_){
+            const response = await Profile.getProfileSpecialities();
+            return response;
+        },
+        async profileTrainersBySpeciality(_,{idSpeciality}){
+            const response = await Profile.getTrainersBySpeciality(idSpeciality);
+            return response;
+        },
+        async profileUser(_,{idUser}){
+            const response = await Profile.getProfileUser(idUser);
+            return response;
+        },
+        async profileTrainer(_,{idTrainer}){
+            const response = await Profile.getProfileTrainer(idTrainer);
+            return response;
+        }
     },
     Mutation:{
         
--- src/typedefs/profile.js
+++ src/typedefs/profile.js
@@ -1,8 +1,53 @@
 export const queries = `
+profileUsers:[profileUser]
+profileTrainers:[profileTrainer]
+profileDegreesByTrainers(idTrainer:ID!):[profileDegree]
+profileSpecialities:[profileSpeciality]
+profileTrainersBySpeciality(idSpeciality:ID!):[profileTrainer]
+profileUser(idUser:ID):profileUser
+profileTrainer(idTrainer:ID!):profileTrainer
 `
 
 export const mutations = `
 `
 
 export const typeDefs =`
+type profileUser{
+    user_id:ID
+    user_name:String
+    age:Int
+    photo:String
+    telephone:String
+    city:String
+}
+
+type profileTrainer{
+    trainer_id:ID
+    trainer_name:String
+    age:Int
+    photo:String
+    telephone:String
+    city:String
+    sum_ratings:Int
+    num_ratings:Int
+    description:String
+    work_experience:String
+    resources:String
+    specialities:[String]
+}
+
+type profileDegree{
+    degree_id:ID
+    degree_name:String
+    year:Int
+    institution:String
+    trainer:Int
+}
+
+type profileSpeciality{
+    speciality_id:ID
+    speciality_name:String
+}
+
+
 `

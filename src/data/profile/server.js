770e6183243b53ba0bf12e6b35962cea548edd5e
Valentina <vbernalb@unal.edu.co>
2020-04-18 19:16:16 -0500

    menos put

---
+++ src/data/profile/server.js
@@ -0,0 +1,6 @@
+const ip = "54.236.130.54";
+export const uriProfileUser = "http://" + ip + ":8000/bs-profile-ms/users"
+export const uriProfileTrainer = "http://" + ip + ":8000/bs-profile-ms/trainers"
+export const uriProfileSpeciaity = "http://" + ip + ":8000/bs-profile-ms/specialities"
+export const uriProfileDegree = "http://" + ip + ":8000/bs-profile-ms/degrees"
+export const uriProfileTrainerSpeciality = "http://" + ip + ":8000/bs-profile-ms/trainerSpecialities"

1b2f55359ef5d2f62cad6211a5ac074f0ee68ddd
unknown <afsanchezsa@unal.edu.co>
2020-04-18 23:00:42 -0500

    first definitions and first query sucess

--- src/resolvers/routine.js
+++ src/resolvers/routine.js
@@ -1,6 +1,12 @@
+
+import * as Routine from "../data/routine/routine";
+import {getAllRoutines} from "../data/routine/routine";
 const resolvers = {
     Query:{
-        
+        async getAllRoutines(){
+            const response=await getAllRoutines();
+            return response.data;
+        }
     },
     Mutation:{
         
--- src/typedefs/routine.js
+++ src/typedefs/routine.js
@@ -1,8 +1,54 @@
 export const queries = `
+getAllRoutines:[Routine]!
 `
 
 export const mutations = `
 `
 
 export const typeDefs =`
+type Request{
+id:ID!,
+routine:Routine!,
+idUser:Int!
+}
+type Resource{
+id:ID!,
+routine:Routine!,
+link:String!,
+title:String!,
+description:String!,
+position:Int!,
+type:TypeResource!
+}
+type Routine{
+id:ID!,
+idOwner:Int!,
+rating:Float!,
+numRaitings:Int!,
+price:Float!,
+name:String!,
+description:String!,
+linkPreview:String!,
+type:TypeRoutine!
+}
+type Status{
+id:ID!,
+name:String!
+}
+type TypeResource{
+id:ID!,
+name:String!
+}
+type TypeRoutine{
+id:ID!,
+name:String!
+}
+type UserRoutine{
+id:ID!,
+idUser:Int!,
+routine:Routine!,
+status:Status!,
+qualified:Boolean!
+}
+
 `

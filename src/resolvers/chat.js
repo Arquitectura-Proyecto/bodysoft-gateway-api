ba3c929a74189dc5d18ee2fa7398d1c52360d790
sv1024 <vejars1024@gmail.com>
2020-04-19 22:00:47 -0500

    authentication and throw errors added

--- src/resolvers/chat.js
+++ src/resolvers/chat.js
@@ -1,5 +1,5 @@
 import * as Chat from "../data/chat/chat";
-
+import * as Authentication from "../data/authentication/authentication";
 const resolvers = {
     Query:{
         hello(){
@@ -23,21 +23,46 @@ const resolvers = {
         }
     },
     Mutation:{
-        async createChatTrainerUser(_,{_id, userId}){
-            const response = await Chat.postTrainerUserChat(_id, userId);
-            return response;
+        async createChatTrainerUser(_,{token, userId }){
+            const responseToken = await Authentication.authValidateAuthToken(token);
+            if(responseToken.TypeID == 1){
+                const response = await Chat.postTrainerUserChat( responseToken.ID, userId);
+                return response;
+            }else{
+                throw new Error("409 not correct type of user");
+            }
+
         },
-        async createChatUserTrainer(_,{_id, trainerId}){
-            const response = await Chat.postUserTrainerChat(_id, trainerId);
-            return response;
+        async createChatUserTrainer(_,{token, trainerId}){
+            const responseToken = await Authentication.authValidateAuthToken(token);
+            if(responseToken.TypeID == 2){
+                const response = await Chat.postUserTrainerChat( responseToken.ID, trainerId);
+                return response;
+            }else{
+                throw new Error("409 not correct type of user");
+            }
+
+
         },
-        async createMessageTrainerUser(_,{_id, chatId, message}){
-            const response = await Chat.postMessageUser(_id, chatId, message);
-            return response;
+        async createMessageTrainerUser(_,{token, chatId, message}){
+            const responseToken = await Authentication.authValidateAuthToken(token);
+            if(responseToken.TypeID == 1){
+                const response = await Chat.postMessageUser(responseToken.ID, chatId, message);
+                return response;
+            }else{
+                throw new Error("409 not correct type of user");
+            }
+
         },
-        async createMessageUserTrainer(_,{_id, chatId, message}){
-            const response = await Chat.postMessageTrainer(_id, chatId, message);
-            return response;
+        async createMessageUserTrainer(_,{token, chatId, message}){
+            const responseToken = await Authentication.authValidateAuthToken(token);
+            if(responseToken.TypeID == 2){
+                const response = await Chat.postMessageTrainer(responseToken.ID, chatId, message);
+                return response;
+            }
+            else{
+                throw new Error("409 not correct type of user");
+            }
         }
     }
 }
